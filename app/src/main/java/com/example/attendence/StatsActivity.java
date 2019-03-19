b5d83916a4ea996a2e16aa2c1ad2543a09794b7a
Sathwika <gonesathwika@gmail.com>
2019-03-18 22:25:51 -0500

    committing changes

--- app/src/main/java/com/example/attendence/StatsActivity.java
+++ app/src/main/java/com/example/attendence/StatsActivity.java
@@ -24,6 +24,37 @@ public class StatsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stats);
         Intent i = getIntent();
+        subjectName = i.getExtras().getString("subjectName");
+        // Toast.makeText(getBaseContext(),subjectName,Toast.LENGTH_SHORT).show();
+        SettingsActivity sa = new SettingsActivity();
+        //-----------------------------------------
+        CONST = getPercent() * 0.01;
+        //Toast.makeText(getBaseContext(),"cal:"+CONST,Toast.LENGTH_SHORT).show();
+        //-----------------------------------------
+        calculatePercantage();
+    }
+
+    void calculatePercantage() {
+        present = 0;
+        absent = 0;
+        off = 0;
+
+        int status = 1;
+        if (status == 1)
+            present++;
+        else if (status == 0)
+            off++;
+        else
+            absent++;
+
+
+        total = present + absent;
+        percent = (present * 100.0) / total;
+        percent = Math.round(percent * 100.0) / 100.0;//rounding to two decimal places
+        pieView = (PieView) findViewById(R.id.pieView);
+        pieView.setInnerTextVisibility(View.VISIBLE);
+        pieView.setInnerText(Double.toString(percent));
+
     }
     public int getPercent() {
         SharedPreferences sp = getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE);
